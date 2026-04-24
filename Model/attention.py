071f1a0c4d4d3f8c3d67baec5a4081568a06d671
Krynovv <139336955+Krynovv@users.noreply.github.com>
2026-04-24 09:53:24 +0300

    last

--- Model/attention.py
+++ Model/attention.py
@@ -15,11 +15,13 @@ class Attention(nn.Module):
    def __init__(self,
                 dim: int,
                 num_heads: int=1,
+                drop: float=0.1,
                 qkv_bias: bool = False,
                 qk_scale: NoneFloat = None):
 
       super().__init__()
 
+      
       self.num_heads = num_heads
       self.chan = dim
       self.head_dim = self.chan // self.num_heads
@@ -28,15 +30,16 @@ class Attention(nn.Module):
 
       self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
       self.proj = nn.Linear(dim, dim)
+      self.attn_drop = nn.Dropout(drop)
 
    def forward(self, x):
       B, N, C = x.shape
-
       qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
       q, k, v = qkv[0], qkv[1], qkv[2]
 
       attn = (q * self.scale) @ k.transpose(-2, -1)
       attn = attn.softmax(dim=-1)
+      attn = self.attn_drop(attn)
 
       x = (attn @ v).transpose(1, 2).reshape(B, N, self.chan)
 
